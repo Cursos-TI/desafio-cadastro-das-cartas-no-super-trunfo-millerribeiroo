6a748e56f7f697f4c7ebf2b99897a304cc5932a1
millerribeiroo <millerrib@gmail.com>
2025-03-19 17:21:08 +0000

    Desafio Novato:
    Foi iniciado o projeto do Supertrunfo, coloquei todos os printf e scanfs necessários
    para coletar a informação das 2 cartas, não foi utilizado o getchar, então o usuário
    deve digitar o número da carta e apertar enter, depois digitar as características subsequentes
    e apertar enter novamente.
    O programa já está compilando e rodando, mas não está fazendo nada ainda.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,16 +4,25 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char estado1, estado2;
+    char carta1[3], carta2[3];
+    char cidade[20], cidade[20];
+    int pop1, pop2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontos1, pontos2;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
+    printf("Insira as informações das 2 cartas que irão competir:\n Pressione 'enter' para continuar.")
+    scanf("")
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
